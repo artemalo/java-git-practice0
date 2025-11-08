e9abd3b538cb0588eb3c9393422faa35b4c17c59
artemalo <artemphirsov@mail.ru>
2025-11-09 01:23:17 +0300

    pipec

--- src/main/java/org/example/InteractiveInput.java
+++ src/main/java/org/example/InteractiveInput.java
@@ -8,17 +8,48 @@ public class InteractiveInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int coin = 0;
+        printMenu();
 
-        while (true) {
+        boolean running = true;
+
+        while (running) {
             try {
-                coin = scanner.nextInt();
-                double amountInDollars = ConvertRUB.convert(coin, Currency.RUB, Currency.DOLLAR);
-                System.out.println(amountInDollars);
+                System.out.print("\nВыберите операцию (1-2): ");
+                int choice = scanner.nextInt();
+                System.out.print("Введите сумму: ");
+                double coin = Double.parseDouble(scanner.next());
+
+                double result;
+                String operation;
+                switch (choice) {
+                    case 1:
+                        result = ConvertRUB.convert(coin, Currency.RUB, Currency.DOLLAR);
+                        operation = "RUB -> USD";
+                        break;
+                    case 2:
+                        result = ConvertRUB.convert(coin, Currency.DOLLAR, Currency.RUB);
+                        operation = "RUB -> USD";
+                        break;
+                    default:
+                        System.out.println("Неверный выбор!");
+                        continue;
+                }
+
+                System.out.printf("%s: %.2f -> %.2f%n", operation, coin, result);
             } catch (IllegalArgumentException e) {
-                System.out.println("Возникла ошибка: " + e.getMessage());
+                System.out.println("Ошибка при ковертации: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Неизвестная ошибка: " + e.getMessage());
+            } finally {
                 System.out.println("Попробуйте снова...");
             }
         }
     }
+
+    private static void printMenu() {
+        System.out.println("=== static КОНВЕРТЕР ВАЛЮТ ===");
+        System.out.println("1. RUB -> USD");
+        System.out.println("2. USD -> RUB");
+    }
+
 }
