ad49901249de88757a35ebe5cca34da3a7492248
artemalo <artemphirsov@mail.ru>
2025-11-09 01:07:01 +0300

    change main - simple input

--- src/main/java/org/example/InteractiveInput.java
+++ src/main/java/org/example/InteractiveInput.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.convert.*;
+
+import java.util.Scanner;
+
 public class InteractiveInput {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int coin = 0;
+
+        while (true) {
+            try {
+                coin = scanner.nextInt();
+                double amountInDollars = ConvertRUB.convert(coin, Currency.RUB, Currency.DOLLAR);
+                System.out.println(amountInDollars);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Возникла ошибка: " + e.getMessage());
+                System.out.println("Попробуйте снова...");
+            }
+        }
     }
 }
