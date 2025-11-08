06fb8e1e615cce1ec5a81f29eb2380cec9b307e6
artemalo <artemphirsov@mail.ru>
2025-11-09 01:05:39 +0300

    edit InteractiveInput.java - make empty

--- src/main/java/org/example/InteractiveInput.java
+++ src/main/java/org/example/InteractiveInput.java
@@ -1,23 +1,7 @@
 package org.example;
 
-import org.example.convert.*;
-
-import java.util.Scanner;
-
 public class InteractiveInput {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int coin = 0;
-
-        while (true) {
-            try {
-
-
-            } catch (IllegalArgumentException)
-            coin = scanner.nextInt();
-            double amountInDollars = ConvertRUB.convert(coin, Currency.RUB, Currency.DOLLAR);
-            System.out.println(amountInDollars);
-        }
+        
     }
 }
