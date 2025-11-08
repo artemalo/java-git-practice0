614ecf40cac2c461eb90187741609f817c54ab2e
artemalo <artemphirsov@mail.ru>
2025-11-09 00:36:12 +0300

    rename Main -> InteractiveInput

--- src/main/java/org/example/InteractiveInput.java
+++ src/main/java/org/example/InteractiveInput.java
@@ -1,8 +1,23 @@
 package org.example;
 
-// User interface
-public class Main {
-    public static void main(String[] args) {
+import org.example.convert.*;
 
+import java.util.Scanner;
+
+public class InteractiveInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int coin = 0;
+
+        while (true) {
+            try {
+
+
+            } catch (IllegalArgumentException)
+            coin = scanner.nextInt();
+            double amountInDollars = ConvertRUB.convert(coin, Currency.RUB, Currency.DOLLAR);
+            System.out.println(amountInDollars);
+        }
     }
 }
